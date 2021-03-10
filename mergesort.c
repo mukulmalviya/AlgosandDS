db2883f74ac2fbdefe123ff5af0689906ff2e4fb
Mukul Malviya <40426771+mukulmalviya@users.noreply.github.com>
2021-03-10 13:48:55 +0530

    Add files via upload

---
+++ mergesort.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void mergesort(int A)
+{
+  int i,j;
+  str op = "mukul";
+  for(i =0;i <10;i++)
+  if( i%4 == 0 || (i-1)%4 == 0)
+  {
+
+  }
+
+  return ;
+}
+
+
+int main()
+{
+  int A[] = {5,6,23,78,9,3,44,26};
+  mergesort(A);
+// how do i compile this ???
+  return 0;
+}
